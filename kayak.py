ad61088b16352749457d409b6ecc5ddf7ac6285a
Odin Nichols <95887189+B1Fr0st@users.noreply.github.com>
2024-08-23 21:12:46 -0700

    prettify

--- kayak.py
+++ kayak.py
@@ -1,3 +1,4 @@
+
 """
 Letter layout
 
@@ -7,6 +8,7 @@ Letter layout
 k a y a k
   k a k
     k
+
 """
 
 letters = [
@@ -16,37 +18,33 @@ letters = [
     ["","k","a","k",""],
     ["","","k","",""],
 ]
-assert len([x for x in letters if len(x) != 5]) == 0
 
 
 
 def get_neighbors(letters, i, j):
     if i > 0:
         top = letters[i-1][j]
-    else:top = ""
+    else:
+        top = ""
     if i < len(letters)-1:
         bottom = letters[i+1][j]
-    else:bottom = ""
+    else:
+        bottom = ""
     if j < len(letters[i])-1:
         right = letters[i][j+1]  
-    else:right = ""
+    else:
+        right = ""
     if j > 0:
         left = letters[i][j-1]
-    else:left = ""
+    else:
+        left = ""
     return [top,bottom,right,left]
-
 
 offsets = [(-1,0),(1,0),(0,1),(0,-1)]
 solutions = []
 for i in range(len(letters)):
     for j in range(len(letters[i])):
         current_letter = letters[i][j]
-        first_k = (0,0)
-        first_a = (0,0)
-        second_a = (0,0)
-        second_k = (0,0)
-
-
         if current_letter == "k": #Find first k
             neighbors = get_neighbors(letters,i,j)
             first_k = (i,j)
@@ -57,20 +55,22 @@ for i in range(len(letters)):
                     temp_j = j+offsets[ind][1]
                     first_a = (temp_i,temp_j)
                     neighbors = get_neighbors(letters,temp_i,temp_j)
+
                     for ind,neighbor in enumerate(neighbors):
                         if neighbor == "y": #find y
                             temp_i2 = temp_i+offsets[ind][0]
                             temp_j2 = temp_j+offsets[ind][1]
                             neighbors = get_neighbors(letters,temp_i2,temp_j2)
+
                             for ind,neighbor in enumerate(neighbors):
                                 temp_i3 = temp_i2+offsets[ind][0]
                                 temp_j3 = temp_j2+offsets[ind][1]
-                                
                                 if (temp_i3,temp_j3) == first_a:
                                     continue
                                 if neighbor == "a":
                                     second_a = (temp_i3,temp_j3)
                                     neighbors = get_neighbors(letters,temp_i3,temp_j3)
+
                                     for ind,neighbor in enumerate(neighbors):
                                         temp_i4 = temp_i3+offsets[ind][0]
                                         temp_j4 = temp_j3+offsets[ind][1]
@@ -97,4 +97,4 @@ for solution in solutions:
         path[index[0]][index[1]] = f"{ind+1}"
     for line in path:
         print(line)
-print(f"number of permutations:{len(solutions)}")
+print(f"number of permutations:{len(solutions)}") #exactly 100 lines makes my soul pure
